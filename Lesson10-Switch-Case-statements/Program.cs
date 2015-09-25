726de883037c620d42562939f9e3db6bd3d65dd5
Arjan de Ruijter <rra@mboutrecht.nl>
2015-09-25 15:25:41 +0200

    Enum

---
+++ Lesson10-Switch-Case-statements/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10_Switch_Case_statements
+{
+
+    class Program
+    {
+        enum mijnKleuren { Onbekend, Pimpelpaars, GeelRood, OranjeBruin };
+
+
+        static void Main(string[] args)
+        {
+            mijnKleuren eigenKleur = mijnKleuren.Onbekend;
+
+            Console.Title = "Lesson 10 Switch Case Statements";
+
+            Console.Write("Geef uw lievelingskleur: ");
+
+            string kleur = Console.ReadLine();
+
+            /*
+            if ( kleur == "paars")
+            {
+                Console.WriteLine("U lievelingskleur is paars");
+            }
+            else if ( kleur == "rood")
+            {
+                Console.WriteLine("U lievelingskleur is rood");
+            }
+            else if (kleur == "groen")
+            {
+                Console.WriteLine("U lievelingskleur is groen");
+            }
+            else if (kleur == "blauw")
+            {
+                Console.WriteLine("U lievelingskleur is blauw");
+            }
+            else if (kleur == "oranje")
+            {
+                Console.WriteLine("U lievelingskleur is oranje");
+            }
+            else
+            {
+                Console.WriteLine("Uw lievelingskleur is niet bekend....");
+            }
+            */
+
+
+            switch ( kleur )
+            {
+                case "GeelRood":
+                    eigenKleur = mijnKleuren.GeelRood;
+                    break;
+                case "OranjeBruin":
+                    eigenKleur = mijnKleuren.OranjeBruin;
+                    break;
+                case "Pimpelpaars":
+                    eigenKleur = mijnKleuren.Pimpelpaars;
+                    break;
+                default:
+                    Console.WriteLine("De gekozen kleur is niet bekend bij de enum mijnKleuren");
+                    break;
+            }
+
+            Console.WriteLine("De gekozen kleur is: {0}", eigenKleur.ToString() );
+
+
+            // Is de kleur groen, rood, blauw of paarse schrijf naar het scherm
+            // Uw lievelingskleur is groen groen, rood, blauw of paarse
+
+            Console.ReadKey();
+        }
+    }
+}
