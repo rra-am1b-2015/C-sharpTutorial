94cc2f91784248fc91a12aafbc27cc7996a83d27
Arjan de Ruijter <rra@mboutrecht.nl>
2015-09-25 13:05:44 +0200

    Keuzemenu

--- Lesson9-DecisionMaking/Program.cs
+++ Lesson9-DecisionMaking/Program.cs
@@ -23,100 +23,106 @@ namespace Lesson9_DecisionMaking
 
             string keuzeInvoer = Console.ReadLine();
 
+           
+            Console.WriteLine("Er zijn {0} letters/cijfers ingevoerd", keuzeInvoer.Length );
+            
             char gekozenLetter = Convert.ToChar(keuzeInvoer);
 
-            if (//test gekozenLetter)
+            if ( gekozenLetter == 'A')
             {
-                //Zet hier alle code m.b.t. het raadspelletje
+                // Code voor het raadspelletje
+
+                Console.WriteLine("Geef een getal tussen de 1 en de 5.\n" +
+                                 "U heeft een kans om het goede getal te raden.\n" +
+                                 "Choose wiseley.........");
+                Console.Write("Geef het getal van uw keuze: ");
+
+                string invoer = Console.ReadLine();
+
+                byte gekozenGetal = Convert.ToByte(invoer);
+
+                if (gekozenGetal == 1)
+                {
+                    Console.WriteLine("U heeft gekozen voor {0}. Gefeliciteerd dit is het winnende getal.\n" +
+                                      "U gaat naar Hawai", gekozenGetal);
+                }
+                else if (gekozenGetal == 0)
+                {
+                    Console.WriteLine("U heeft gekozen voor {0}. Gefeliciteerd dit is ook het winnende getal.\n" +
+                                      "U gaat naar Texel", gekozenGetal);
+                }
+                else if (gekozenGetal == 4)
+                {
+                    Console.WriteLine("U heeft gekozen voor {0}. Gefeliciteerd, dit is ook een winnend getal,\n " +
+                                      "u gaat naar Terschelling", gekozenGetal);
+                }
+                else
+                {
+                    Console.WriteLine("U heeft gekozen voor {0}. Dit is niet het winnende getal. Helaas,\n" +
+                                      "volgende keer beter.", gekozenGetal);
+                }
+
+
             }
-            else if (// test gekozenLetter)
+            else if ( gekozenLetter == 'B' )
             {
-                // Zet hier alle code voor de BMI Berekening
+                // Hier staat alle code voor de BMI Berekening
+                Console.WriteLine("\n\nDit programma berekent de BMI (Body Mass Index)");
+
+                Console.Write("Geef uw lengte in meters: ");
+
+                string invoer = Console.ReadLine();
+
+                float lengteInMeters = Convert.ToSingle(invoer);
+
+                Console.Write("Geef uw gewicht in kilogrammen: ");
+
+                invoer = Console.ReadLine();
+
+                float gewichtInKilogram = Convert.ToSingle(invoer);
+
+                float bmi = gewichtInKilogram / (lengteInMeters * lengteInMeters);
+
+                double bmiRounded = Math.Round(Convert.ToDouble(bmi), 1);
+
+                Console.WriteLine("Uw BMI bedraagt op dit moment: {0}", bmiRounded.ToString());
+
+                if (bmiRounded < 18.5)
+                {
+                    Console.WriteLine("U heeft een BMI van {0}, U heeft ondergewicht", bmiRounded);
+                }
+                else if (bmiRounded >= 18.5 && bmiRounded < 25)
+                {
+                    Console.WriteLine("U heeft een BMI van {0}, U heeft normaal gewicht", bmiRounded);
+                }
+                else if (bmiRounded >= 25 && bmiRounded < 27)
+                {
+                    Console.WriteLine("U heeft een BMI van {0}, U heeft licht overgewicht", bmiRounded);
+                }
+                else if (bmiRounded >= 27 && bmiRounded < 30)
+                {
+                    Console.WriteLine("U heeft een BMI van {0}, U heeft matig overgewicht", bmiRounded);
+                }
+                else if (bmiRounded >= 30 && bmiRounded < 40)
+                {
+                    Console.WriteLine("U heeft een BMI van {0}, U heeft ernstig overgewicht", bmiRounded);
+                }
+                else
+                {
+                    Console.WriteLine("U heeft een BMI van {0}, U heeft ziekelijk overgewicht", bmiRounded);
+                }
 
             }
             else
             {
                 // Zeg hier wat je gaat doen als de keuze niet A of B is.
+                Console.WriteLine("U heeft een niet-bestaande keuze gemaakt. Het programma sluit......");
             }
 
 
-            Console.WriteLine("Geef een getal tussen de 1 en de 5.\n" +
-                              "U heeft een kans om het goede getal te raden.\n" +
-                              "Choose wiseley.........");
-            Console.Write("Geef het getal van uw keuze: ");
+           
 
-            string invoer = Console.ReadLine();
-
-            byte gekozenGetal = Convert.ToByte(invoer);
-
-            if (gekozenGetal == 1)
-            {
-                Console.WriteLine("U heeft gekozen voor {0}. Gefeliciteerd dit is het winnende getal.\n" + 
-                                  "U gaat naar Hawai", gekozenGetal);
-            }
-            else if (gekozenGetal == 0)
-            {
-                Console.WriteLine("U heeft gekozen voor {0}. Gefeliciteerd dit is ook het winnende getal.\n" +
-                                  "U gaat naar Texel", gekozenGetal);
-            }
-            else if (gekozenGetal == 4)
-            {
-                Console.WriteLine("U heeft gekozen voor {0}. Gefeliciteerd, dit is ook een winnend getal,\n " + 
-                                  "u gaat naar Terschelling",gekozenGetal );
-            }
-            else
-            {
-                Console.WriteLine("U heeft gekozen voor {0}. Dit is niet het winnende getal. Helaas,\n" + 
-                                  "volgende keer beter.", gekozenGetal);
-            }
-
-
-
-            Console.WriteLine("\n\nDit programma berekent de BMI (Body Mass Index)");
-
-            Console.Write("Geef uw lengte in meters: ");
-
-            invoer = Console.ReadLine();
-
-            float lengteInMeters = Convert.ToSingle(invoer);
-
-            Console.Write("Geef uw gewicht in kilogrammen: ");
-
-            invoer = Console.ReadLine();
-
-            float gewichtInKilogram = Convert.ToSingle(invoer);
-
-            float bmi = gewichtInKilogram / (lengteInMeters * lengteInMeters);
-
-            double bmiRounded = Math.Round(Convert.ToDouble(bmi), 1);
-
-            Console.WriteLine("Uw BMI bedraagt op dit moment: {0}", bmiRounded.ToString());
-
-            if ( bmiRounded < 18.5 )
-            {
-                Console.WriteLine("U heeft een BMI van {0}, U heeft ondergewicht", bmiRounded);
-            }
-            else if ( bmiRounded >= 18.5  &&   bmiRounded < 25 )
-            {
-                Console.WriteLine("U heeft een BMI van {0}, U heeft normaal gewicht", bmiRounded);
-            }
-            else if ( bmiRounded >= 25 && bmiRounded < 27 )
-            {
-                Console.WriteLine("U heeft een BMI van {0}, U heeft licht overgewicht", bmiRounded);
-            }
-            else if ( bmiRounded >= 27 && bmiRounded < 30 )
-            { 
-                Console.WriteLine("U heeft een BMI van {0}, U heeft matig overgewicht", bmiRounded);
-            }
-            else if ( bmiRounded >= 30 && bmiRounded < 40 )
-            {
-                Console.WriteLine("U heeft een BMI van {0}, U heeft ernstig overgewicht", bmiRounded);
-            }
-            else
-            {
-                Console.WriteLine("U heeft een BMI van {0}, U heeft ziekelijk overgewicht", bmiRounded);
-            }
-
+           
 
 
             Console.ReadKey();
