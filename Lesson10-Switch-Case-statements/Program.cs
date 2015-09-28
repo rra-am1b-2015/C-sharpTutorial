483193b1a706ba6309710b4af6c65ae19ef73a3e
Arjan de Ruijter <rra@mboutrecht.nl>
2015-09-28 14:40:01 +0200

    While loop

--- Lesson10-Switch-Case-statements/Program.cs
+++ Lesson10-Switch-Case-statements/Program.cs
@@ -6,15 +6,18 @@ using System.Threading.Tasks;
 
 namespace Lesson10_Switch_Case_statements
 {
-
+    
 
     class Program
     {
         enum mijnKleuren { Onbekend, Pimpelpaars, GeelRood, OranjeBruin };
-        
+        enum auto { Fiat, Mercedes, Audi, Ford, Volkswagen };
+
         static void Main(string[] args)
         {
-            enum auto { Fiat, Mercedes };
+            int getal = 10;
+
+            auto autoMerk = auto.Fiat;
 
             mijnKleuren eigenKleur = mijnKleuren.Onbekend;
 
@@ -79,6 +82,32 @@ namespace Lesson10_Switch_Case_statements
             // Maak een variabele autoMerk van het type enum auto.
             // Geef deze variabele een waarde auto.Fiat
             // Lees de waarde van autoMerk uit met een switch case. En echo welk merk gekozen is.
+
+            switch ( autoMerk )
+            {
+                case auto.Audi:
+                    Console.WriteLine("U heeft een {0}", autoMerk.ToString());
+                    break;
+                case auto.Fiat:
+                    Console.WriteLine("U heeft een {0}", autoMerk.ToString());
+                    break;
+                case auto.Ford:
+                    Console.WriteLine("U heeft een {0}", autoMerk.ToString());
+                    break;
+                case auto.Mercedes:
+                    Console.WriteLine("U heeft een {0}", autoMerk.ToString());
+                    break;
+                case auto.Volkswagen:
+                    Console.WriteLine("U heeft een {0}", autoMerk.ToString());
+                    break;
+                default:
+                    Console.WriteLine("De door uw gekozen waarde is niet bekend in de enum");
+                    break;
+            }
+
+            // Maak een enum genaamd weekdagen. weekdagen kan alle dagen van de week aannemen
+            // Maak een variabele vandaag. Geef deze de waarde weekdagen.Vrijdag
+            // Bekijk vandaag in een switch case en schrijf naar het scherm. Het is vandaag Vrijdag.
             Console.ReadKey();
         }
     }
---
+++ Lesson11-While-Loop/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11_While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Lesson 11 De While Loop";
+
+            int getal = 0;
+            /*
+            Console.WriteLine("0");
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("3");
+            Console.WriteLine("4");
+            Console.WriteLine("5");
+            Console.WriteLine("6");
+            Console.WriteLine("7");
+            Console.WriteLine("8");
+            Console.WriteLine("9");
+            Console.WriteLine("10");
+            */
+            // Zolang de variabele getal kleiner of gelijk is aan 10 mag het worden geschreven naar het scherm
+
+            while ( getal < 11 )
+            {
+                Console.WriteLine(Convert.ToString(getal));
+                //getal = getal + 1;
+                getal++;
+                //getal += 1;
+                //getal += 4;
+            }
+
+            getal = 10;
+
+            while ( getal > 0 )
+            {
+                Console.WriteLine(getal.ToString());
+                //getal = getal - 1;
+                //getal--;
+                getal -= 1;
+                //getal -= 4;
+            }
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
