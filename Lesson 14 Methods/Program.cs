126aa6e62c31b649ab324a09e3ab8fd6316c7f6f
Arjan de Ruijter <rra@mboutrecht.nl>
2015-10-16 15:24:28 +0200

    methods

---
+++ Lesson 14 Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_14_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Lesson 14 Methods";
+
+            for (int i = 0; i < 100; i++)
+            {
+                //groeten();
+            }
+
+            groeten(100, "de heer","de Ruijter");
+            groeten(2, "de heer", "Odijk");
+            groeten(1200, "mevrouw", "Hombergh");
+
+
+            Console.ReadKey();
+        }
+
+        static void groeten(int aantal, string geslacht, string naam)
+        {
+            Console.WriteLine("{0} x felicitaties van {1} {2}", aantal, geslacht, naam);
+        }
+    }
+}
