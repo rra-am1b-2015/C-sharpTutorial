29efcd4694a8704b8aa0e7b45f4415852520455c
Arjan de Ruijter <rra@mboutrecht.nl>
2015-10-16 15:31:48 +0200

    methods

--- Lesson 14 Methods/Program.cs
+++ Lesson 14 Methods/Program.cs
@@ -21,13 +21,27 @@ namespace Lesson_14_Methods
             groeten(2, "de heer", "Odijk");
             groeten(1200, "mevrouw", "Hombergh");
 
+            // Maak een method die twee string argumenten heeft en deze aan elkaar plakt.
+
+            Console.WriteLine("Dit is de aan elkaar geplakte string: {0}", plakString("Hallo", "en dag"));
+
 
             Console.ReadKey();
         }
+
+
 
         static void groeten(int aantal, string geslacht, string naam)
         {
             Console.WriteLine("{0} x felicitaties van {1} {2}", aantal, geslacht, naam);
         }
+
+        static string plakString(string string1, string string2)
+        {
+            string nieuweString = string1 + string2;
+            return nieuweString;
+        }
+
+
     }
 }
