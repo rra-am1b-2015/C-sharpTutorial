fe541b62db1c9b7c5aed1d6a78d3894c095a70af
Arjan de Ruijter <rra@mboutrecht.nl>
2015-10-30 15:39:40 +0100

    class Begroeting

---
+++ Lesson16-ClassesPart2/Begroeting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson16_ClassesPart2
+{
+    class Begroeting
+    {
+        //Fields
+        string naam = "Bertje";
+        int leeftijd = 16;
+
+        //Constructor
+        public Begroeting()
+        {
+        }
+
+        public Begroeting(string naam)
+        {
+            this.naam = naam;
+        }
+
+        // Methods
+        public void gegroet()
+        {
+            Console.WriteLine("Ik vind jouw een toffe peer! " + this.naam + " voor je leeftijd ({0} jaar)",
+                                this.leeftijd);
+        }
+    }
+}
---
+++ Lesson16-ClassesPart2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson16_ClassesPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Lesson 16 Classes Part 2";
+            Begroeting zegHallo;
+            zegHallo = new Begroeting("Fransje");
+            zegHallo.gegroet();
+
+            // Maak verschillende Begroeting objecten (instanties) die verschillende personen begroeten.
+            // Voeg een veld leeftijd toe. Maak een nieuwe constructor die zowel de naam als de leeftijd kan
+            // initialiseren.
+
+            Begroeting zegGedag = new Begroeting("Suzanne");
+            zegGedag.gegroet();
+
+
+            Console.ReadKey();
+        }
+    }    
+}
+
+
