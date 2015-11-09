ba2f18c59f4bf4c88756c7ca8648888316dee023
Arjan de Ruijter <rra@mboutrecht.nl>
2015-11-09 15:26:09 +0100

    Classes 5

--- Lesson16-ClassesPart5/Program.cs
+++ Lesson16-ClassesPart5/Program.cs
@@ -12,6 +12,32 @@ namespace Lesson16_ClassesPart5
         {
             Console.Title = "Lesson 16 Classes Part 5";
 
+            Smartphone mobieltje;
+            mobieltje = new Smartphone("Samsung", 12.56f, OperatingSystem.Android, "Kitkat", 16 );
+            mobieltje.ShowSmartphoneProps();
+            Console.WriteLine(mobieltje.ShowSmartphoneInfoText());
+
+            Smartphone[] mobieltjes = new Smartphone[] 
+            {
+                new Smartphone("Samsung", 12.56f, OperatingSystem.Android, "Kitkat", 16),
+                new Smartphone("IPhone", 230f, OperatingSystem.iOS, "iOS 7", 32),
+                new Smartphone("BlackBerry", 670f, OperatingSystem.BlackberryOs, "BlackBerry OS 10", 64),
+                new Smartphone("Blackberry", 430f, OperatingSystem.BlackberryOs, "Blackberry OS 9", 128),
+                new Smartphone("Samsung", 30f, OperatingSystem.Android, "LolliPop", 128),
+            };
+
+            for (int i = 0; i < mobieltjes.Length; i++)
+            {
+                //Console.WriteLine(mobieltjes[i].ShowSmartphoneInfoText());
+            }
+
+            foreach (Smartphone mobile in mobieltjes)
+            {
+                mobile.ShowSmartphoneProps();
+            }
+
+
+
             // Een smartphone class in een apart bestand
             // bedenk 5 relevante Fields
             // Maak de properties voor de Fields (get en set)
@@ -19,6 +45,15 @@ namespace Lesson16_ClassesPart5
             // Maak een method die alle relevante gegevens weer kan geven.
             // Maak een smartphone array met 5 objecten.
             // Roep met een foreach loop de method ShowSmartPhone() aan van alle objecten.
+
+
+            // Maak een laptop class in een apart bestand
+            // bedenk 5 relevante Fields
+            // Maak de properties voor de Fields (get en set)
+            // Maak een constructor die alle 5 de Fields kan initialiseren.
+            // Maak een method die alle relevante gegevens weer kan geven.
+            // Maak een laptop array met 5 objecten.
+            // Roep met een foreach loop de method ShowLaptop() aan van alle objecten.
             Console.ReadKey();
         }
     }
---
+++ Lesson16-ClassesPart5/Smartphone.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson16_ClassesPart5
+{
+    enum OperatingSystem { Android, iOS, BlackberryOs, WindowsPhone };
+
+    class Smartphone
+    {
+        // Fields
+        string brand = "Samsung";
+        float price = 0.00f;
+        OperatingSystem operatingSystem = OperatingSystem.Android;
+        string versionOS = "Lollipop";
+        short memoryCapacity = 4;
+        
+        // Properties
+        public string Brand
+        {
+            get { return this.brand; }
+            set { this.brand = value; }
+        }
+        public float Price
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
+        public OperatingSystem OperatingSystem
+        {
+            get { return this.operatingSystem; }
+            set { this.operatingSystem = value; }
+        }
+        public string VersionOs
+        {
+            get { return this.versionOS; }
+            set { this.versionOS = value; }
+        }
+        public short MemoryCapacity
+        {
+            set { this.memoryCapacity = value; }
+            get { return this.memoryCapacity; }
+        }
+
+        // Constructor
+        public Smartphone(string brand, float price, OperatingSystem operatingSystem, string versionOS, short memoryCapacity)
+        {
+            this.brand = brand;
+            this.price = price;
+            this.operatingSystem = operatingSystem;
+            this.versionOS = versionOS;
+            this.memoryCapacity = memoryCapacity;
+        }
+
+        public void ShowSmartphoneProps()
+        {
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++\n" +
+                              "Het merk is\t\t\t: {0}\n" +
+                              "De prijs is\t\t\t: {1}\n" +
+                              "Het besturingssysteem is\t: {2}\n" +
+                              "De versie van het OS is\t\t: {3}\n" +
+                              "De grote van het geheugen is\t: {4}\n" +
+                              "+++++++++++++++++++++++++++++++++++++++++++++++\n", 
+                                  this.brand,
+                                  this.price,
+                                  this.operatingSystem,
+                                  this.versionOS,
+                                  this.memoryCapacity);
+        }
+
+        public string ShowSmartphoneInfoText()
+        {
+            string infoText = string.Format("+++++++++++++++++++++++++++++++++++++++++++++++\n" +
+                                            "Het merk is\t\t\t: {0}\n" +
+                                            "De prijs is\t\t\t: {1}\n" +
+                                            "Het besturingssysteem is\t: {2}\n" +
+                                            "De versie van het OS is\t\t: {3}\n" +
+                                            "De grote van het geheugen is\t: {4}\n" +
+                                            "+++++++++++++++++++++++++++++++++++++++++++++++\n",
+                                            this.brand,
+                                            this.price,
+                                            this.operatingSystem,
+                                            this.versionOS,
+                                            this.memoryCapacity);
+            return infoText;
+        }
+
+        // Methods
+    }
+}
