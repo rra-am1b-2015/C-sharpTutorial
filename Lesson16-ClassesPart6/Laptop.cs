d6544b321958b8a5b767d60d4eea2b2f634c8657
Arjan de Ruijter <rra@mboutrecht.nl>
2015-11-13 15:45:30 +0100

    Write to file

---
+++ Lesson16-ClassesPart6/Laptop.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson16_ClassesPart6
+{
+    enum video { AMD_RADEON, INTEL_HD_GRAPHICS, NVIDIA_GEFORCE, NVIDIA_TEGRA, AMD_FIREPRO };
+
+    class Laptop
+    {
+        //Fields
+        private string brand;
+        private float displayDiagonal;
+        private string processor;
+        private int internalMemory;
+        private video card;
+
+        //Properties
+        public string Brand
+        {
+            get { return this.brand; }
+            set { this.brand = value; }
+        }
+        public float DisplayDiagonal
+        {
+            get { return this.displayDiagonal; }
+            set { this.displayDiagonal = value; }
+        }
+        public string Processor
+        {
+            get { return this.processor; }
+            set { this.processor = value; }
+        }
+        public int InternalMemory
+        {
+            get { return this.internalMemory; }
+            set { this.internalMemory = value; }
+        }
+        public video Card
+        {
+            get { return this.card; }
+            set { this.card = value; }
+        }
+
+        //Constructor
+        public Laptop(string brand, float displayDiagonal, string processor, int internalMemory, video card)
+        {
+            this.brand = brand;
+            this.displayDiagonal = displayDiagonal;
+            this.processor = processor;
+            this.internalMemory = internalMemory;
+            this.card = card;
+        }
+
+        //Methods
+        public void ShowLaptopPropertiesConsole()
+        {
+            Console.WriteLine("\r\n++++++++++++++++++++++++++++++++++++++++++++++++++\r\n" +
+                              "Het merk is:\t\t\t{0}\r\n" +
+                              "De beeldscherm diagonaal is:\t{1} INCH\r\n" +
+                              "De processor is:\t\t{2}\r\n" +
+                              "Grootte interngeheugen:\t\t{3}\r\n" +
+                              "Type videokaart:\t\t{4}\r\n" +
+                              "++++++++++++++++++++++++++++++++++++++++++++++++++\r\n", 
+                              this.brand, 
+                              this.displayDiagonal,
+                              this.processor,
+                              this.internalMemory,
+                              this.card);
+        }
+
+        public static void ShowLaptops(Laptop[] laptops)
+        {
+            for (int i = 0; i < laptops.Length; i++)
+            {
+                laptops[i].ShowLaptopPropertiesConsole();
+            }
+        }
+
+        public static void WriteToFile(Laptop[] laptops)
+        {
+            string dir = Directory.GetCurrentDirectory();
+            string outputDir = dir + @"\output";
+            Directory.CreateDirectory(outputDir);
+            string output = "";
+            for( int i = 0; i < laptops.Length; i++ )
+            {
+                output += string.Format("\r\n++++++++++++++++++++++++++++++++++++++++++++++++++\r\n" +
+                                        "Het merk is:\t\t\t{0}\r\n" +
+                                        "De beeldscherm diagonaal is:\t{1} INCH\r\n" +
+                                        "De processor is:\t\t{2}\r\n" +
+                                        "Grootte interngeheugen:\t\t{3}\r\n" +
+                                        "Type videokaart:\t\t{4}\r\n" +
+                                        "++++++++++++++++++++++++++++++++++++++++++++++++++\r\n",
+                                        laptops[i].Brand,
+                                        laptops[i].DisplayDiagonal,
+                                        laptops[i].Processor,
+                                        laptops[i].InternalMemory,
+                                        laptops[i].Card);
+            }
+           
+            File.WriteAllText(outputDir + @"\test.txt", output );
+            Console.WriteLine("{0}", "Hallo");
+        }
+    }
+}
---
+++ Lesson16-ClassesPart6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson16_ClassesPart6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Maak een laptop class in een apart bestand
+            // bedenk 5 relevante Fields
+            // Maak de properties voor de Fields (get en set)
+            // Maak een constructor die alle 5 de Fields kan initialiseren.
+            // Maak een method die alle relevante gegevens weer kan geven in de console (ShowLaptopPropertiesConsole()).
+            // Maak een laptop array met 5 objecten.
+            // Roep met een foreach loop de method ShowLaptop() aan van alle objecten.
+            // Schrijf de inhoud van het Laptops array naar een tekstbestand output.txt
+
+            Console.Title = "Lesson 16 Classes Part 6";
+            Laptop mijnLaptop = new Laptop("HP", 15.6f, "AMD A8", 8, video.NVIDIA_TEGRA);
+            mijnLaptop.Brand = "LENOVO";
+            mijnLaptop.ShowLaptopPropertiesConsole();
+
+            Laptop[] laptops = new Laptop[5]
+            {
+                   new Laptop("HP", 15.6f, "AMD A8", 8, video.NVIDIA_TEGRA),
+                   new Laptop("LENOVO", 11.3f, "INTEL", 16, video.AMD_FIREPRO),
+                   new Laptop("ASUS", 21.0f, "NOBROWSKI", 32, video.AMD_RADEON),
+                   new Laptop("MSI", 14.8f, "INSIT", 4, video.INTEL_HD_GRAPHICS),
+                   new Laptop("APPLE", 7f, "TELMATIC", 2, video.NVIDIA_GEFORCE)
+            };
+
+            // Static aanroepen van een static method
+            Laptop.ShowLaptops(laptops);
+
+            Laptop.WriteToFile(laptops);
+
+        Console.ReadKey();
+        }
+    }
+}
